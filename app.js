9e860eb1e23d6927bd2300c70a9c00126e0ffafc
unknown <rubendaj@msn.com>
2021-05-18 20:19:29 -0500

    Agregando archivo procfile

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express ();
 const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 
-app.use(express.static(publicPath));
+const port = process.env.PORT || 3000
 
-app.listen(3000, ()=> {
+app.use(express.static(publicPath))
+
+app.listen(port, ()=> {
     console.log('Corriendo Servidor')
 });
 
